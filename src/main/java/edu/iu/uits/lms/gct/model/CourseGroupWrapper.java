c45a523a254533a3788f007672acfc7e3d02068b
chmaurer <chmaurer@iu.edu>
2021-07-21 16:31:54 -0400

    LMSA-7553 CourseGroupWrapper needs to be serializable for redis

--- src/main/java/edu/iu/uits/lms/gct/model/CourseGroupWrapper.java
+++ src/main/java/edu/iu/uits/lms/gct/model/CourseGroupWrapper.java
@@ -2,11 +2,12 @@ package edu.iu.uits.lms.gct.model;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class CourseGroupWrapper {
+public class CourseGroupWrapper implements Serializable {
    private SerializableGroup allGroup;
    private SerializableGroup teacherGroup;
    private List<SerializableGroup> canvasGroups = new ArrayList<>();
